9eea9289384455f66d5cb417ef05083fe9c709f4
Mateo Lardiez <mateo.lardiez@bayer.com>
2025-01-16 13:56:21 -0300

    adding simple non-functioning navbar

---
+++ assistant/src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <button>Intro</button>
+            <button>Chatbots</button>
+            <button>Footer</button>
+        </nav>
+    )
+}
+
+export default Navbar;
